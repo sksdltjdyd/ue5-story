20f9bda30d537f644acad8eb16aa8024066eb5f2
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-05-07 08:21:13 +0900

    Update test_onnxexport.py

--- ch2 - unreal-nne/test_onnxexport.py
+++ ch2 - unreal-nne/test_onnxexport.py
@@ -14,3 +14,23 @@ model.eval()
 
 # 모델 구조 확인
 print(model)
+
+# 가짜 데이터(dummy input) 만들기
+# 언리얼에 패턴을 알려주는 쓰레기값  데이터
+dummy_input = torch.rand(1,3,224,224)
+
+# onnx 파일로 추출(언리얼 프로젝트 폴더에 넣을 파일)
+onnx_file_path ="resnet50_unreal.onnx"
+torch.onnx.export(
+    model,                          # 1. 굽고 싶은 모델 객체
+    dummy_input,                    # 2. 길을 뚫어줄 가짜 데이터
+    "my_model.onnx",                # 3. 저장될 파일명
+    export_params=True,             # (가중치/뇌 포함 여부. 항상 True)
+    opset_version=14,               # 4. 언리얼 NNE가 잘 읽을 수 있는 FBX 버전
+    input_names=['input_image'],    # 5. 언리얼 C++에서 접근할 입력 변수명
+    output_names=['output_score'],  # 6. 언리얼 C++에서 접근할 출력 변수명
+    dynamic_axes={                  # 7. (옵션) 0번 차원(배치)은 가변 개수로 열어두기
+        'input_image': {0: 'batch_size'},    
+        'output_score': {0: 'batch_size'}
+    }
+)
